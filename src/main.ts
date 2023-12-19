c6df0e9864dd93dd475fc2d591fadc4a041b843e
Nurkhat Muratkan <120664100+nurhatmurathan@users.noreply.github.com>
2023-12-20 03:15:27 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: true,
+        origin: '*',
       });
     const config = new DocumentBuilder()
         .addBearerAuth()
