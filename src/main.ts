e4544e7eec6e4aa226ad4dbc272107a9dfa8e8e5
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2023-12-20 08:30:34 +0600

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,16 @@ async function bootstrap() {
     dotenv.config();
 
     const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
-    app.enableCors({ allowedHeaders: ["http://*", "https://*", "*"] });
+    // <<<<<<< HEAD
+    //     app.enableCors({ allowedHeaders: ["http://*", "https://*", "*"] });
 
-=======
-    app.enableCors({
-         origin: ['http://localhost:3000'],
-  methods: ['GET', 'POST'],
-  credentials: true,
-      });
->>>>>>> 5301a972fe2b7fa7b067203c0baaab77725f3d5f
+    // =======
+    //     app.enableCors({
+    //          origin: ['http://localhost:3000'],
+    //   methods: ['GET', 'POST'],
+    //   credentials: true,
+    //       });
+    // >>>>>>> 5301a972fe2b7fa7b067203c0baaab77725f3d5f
     const config = new DocumentBuilder()
         .addBearerAuth()
         .setTitle("Game Of Drones")
