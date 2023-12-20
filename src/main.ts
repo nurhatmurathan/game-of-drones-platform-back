d0bde34696cfded7fe64f6a791a9c6dcad56868c
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2023-12-20 09:36:09 +0600

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,7 @@ async function bootstrap() {
     app.enableCors({
         origin: "*",
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-        exposedHeaders: ["Authorization"],
+        allowedHeaders: "Content-Type, Accept",
     });
 
     const config = new DocumentBuilder()
