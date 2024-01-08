7a5b546139c8793ab4433200e896b3ce42089d01
AferDust <aferdust27@gmail.com>
2024-01-08 17:23:07 +0600

    deploy: adding callback urls to dockerfile

--- src/auth/strategies/facebook.strategy.ts
+++ src/auth/strategies/facebook.strategy.ts
@@ -11,7 +11,7 @@ export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
         super({
             clientID: process.env.FACEBOOK_APP_ID,
             clientSecret: process.env.FACEBOOK_APP_SECRET,
-            callbackURL: 'http://localhost:3000/auth/facebook/callback',
+            callbackURL: process.env.FACEBOOK_CALLBACK_URL,
             profileFields: ['name', 'email', 'photos'],
             scope: ['email', 'public_profile'],
         });
