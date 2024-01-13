85b1f1358cfbf4341fe979f64df25e118ec4cbc3
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2024-01-13 21:45:38 +0600

    feat: dron entity

--- src/entities/dron/dron.module.ts
+++ src/entities/dron/dron.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserModule } from "../user/user.module";
 import { Dron } from "./dron.entity";
 import { DronService } from "./dron.service";
+import { DronController } from './dron.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Dron]), UserModule],
     providers: [DronService],
     exports: [DronService],
+    controllers: [DronController],
 })
 export class DronModule {}
