48fbbeeb9a53a7118789ff8c8601593d636f35be
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2024-02-07 16:29:54 +0600

    feat : training validate

--- src/entities/training/training.controller.ts
+++ src/entities/training/training.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Request, UseGuards } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    HttpCode,
+    HttpStatus,
+    Param,
+    ParseIntPipe,
+    Post,
+    Request,
+    UseGuards,
+} from "@nestjs/common";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { CustomAuthGuard } from "../../auth/guards/auth.guard";
 
+import { TrainingGameDto } from "./dto";
 import { TrainingService } from "./training.service";
 
 @ApiTags("Training")
@@ -9,7 +20,7 @@ import { TrainingService } from "./training.service";
 @ApiBearerAuth()
 @UseGuards(CustomAuthGuard)
 export class TrainingController {
-    constructor(private readonly trainingService: TrainingService) { }
+    constructor(private readonly trainingService: TrainingService) {}
 
     // @Get("trainings/:tournamentId")
     // @ApiResponse({ isArray: true })
@@ -27,5 +38,17 @@ export class TrainingController {
     ): Promise<any> {
         console.log("I'm in addTraining");
         return this.trainingService.addTraining(request.user.sub, tournamentId, trainingId);
+    }
+
+    @Post("validate-training")
+    @ApiBearerAuth()
+    @HttpCode(HttpStatus.ACCEPTED)
+    @UseGuards(CustomAuthGuard)
+    async validateTournament(@Body() startGameDto: TrainingGameDto, @Request() req): Promise<any> {
+        return this.trainingService.trainingStartedAndExistsValidator(
+            req.user.sub,
+            startGameDto.id,
+            startGameDto.tournamentId
+        );
     }
 }
